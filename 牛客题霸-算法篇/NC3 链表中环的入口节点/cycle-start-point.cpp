9223f900394644e7fad3ee07f849a8bd436bf492
xuedong <shang.xuedong@yahoo.fr>
2021-02-25 11:35:56 +0100

    Add NC3

---
+++ 牛客题霸-算法篇/NC3 链表中环的入口节点/cycle-start-point.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        bool cycle = false;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (slow && fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+        
+        if (cycle) {
+            ListNode* current = head;
+            while (current != slow) {
+                current = current->next;
+                slow = slow->next;
+            }
+            return slow;
+        }
+        
+        return NULL;
+    }
+};
+
